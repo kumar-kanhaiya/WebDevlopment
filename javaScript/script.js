426c20bafd1bbbcb0cf445f7db79181d04f94d35
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-06-22 19:47:35 +0530

    code

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,4 +1,38 @@
+// types of variables 
 console.log("hello world !!")
 // its helpp to print something 
+// let name = "kanhaiya"
+// let radius = 6;
+// x = null;
+// y = undefined;
+// null means kuch to hai lakin pata nhi filhal
+// undefined matlab pata nhi kya hai 
+// alert(radius)
+// isfollow = true;
+// isfollow = false; //bollean type 
+// console.log( " hello " + name)
+// Variable types
+// var : variable can be re-declared & updated . A global scope variable 
+// let : Variable cannot be re-declared but can be updated. A block scope variables. 
+//  const : variable cannot be re-declared or updated. A block scope variable 
 let name = "kanhaiya"
-console.log( " hello " + '~{$name}')
+console.log("hello " + name)
+// Data type
+
+// number , string , boolean , undefined , null , bigint , symbol
+// premitive data types upper are all premitive datat ypes
+
+// Non premitive array object 
+const student = {
+    Name: "kanhaiya singh ",
+    age: 20 ,
+    cgps: 8.2,
+};
+console.log(student)
+// this is comment it is not a part of the code 
+
+// operators in Js 
+// used to perform some operation on data 
+// Arithmetic operators +,-,*,/,%
+console.log(5+9)
+
