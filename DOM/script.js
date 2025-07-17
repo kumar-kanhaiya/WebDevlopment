dd5969bc92f73bfe70882a202c73fc29dc7549aa
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-17 13:32:26 +0530

    code

---
+++ DOM/script.js
@@ -0,0 +1,10 @@
+/*
+DOM manipulation 
+1. Selecting with id 
+document.getElementbyId("id name ")
+2. Selecting with class 
+document.getElementByClassName("class name ")
+3. Selecting with tag
+document.getElementByTagName("here is the tag name ")
+
+*/
