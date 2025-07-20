040af572c4e758ba36c8ea30c9278c9e43c8bb2f
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-20 09:25:56 +0530

    code

--- DOM/script.js
+++ DOM/script.js
@@ -28,6 +28,24 @@ innerHTML : return the plain text or html contents in the element
 textContent : return textual content even for hidden elements
 
 */
-let div = document.querySelector("h1");
-let button = document.querySelector("#Submit")
-butoon = div.innerText = "kanhaiya kumar ";
+// let div = document.querySelector("h1");
+// let button = document.querySelector("#Submit")
+// butoon = div.innerText = "kanhaiya kumar ";
+
+// part -2
+/*
+dom atributes 
+getAttribute(attr) // to get the attribute value
+setAttribute(attr , value ) // to set the attribute val th 
+style
+node.style
+// inset element 
+1. node.append(el) // adds at the end of node
+2. node.prepend(el) // adds at the start of node
+3.  node.before(el) // adds before the node(outside)
+4. node.agter(el) // adds after the node (outside )
+ 
+delete element 
+node.remove
+
+*/
