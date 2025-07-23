6d76656137bfc11e62a3a8d4b527b6c632a85899
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-24 04:52:12 +0530

    codE

--- classesAndCallback/script.js
+++ classesAndCallback/script.js
@@ -96,5 +96,23 @@ function getData(data){
     },2000);
 }
 getData(1)
-getData(2)
-getData(3)
+
+/// waiting approach 
+function getData(dataId , getNextData){
+    setTimeout(()=>{
+        console.log("data",dataId)
+        if(getNextData){
+
+            getNextData();
+        }
+
+    },2000);
+}
+// callback hell -- nested callbacks 
+getData(1,()=>{
+    getData(2,()=>{
+        getData(3)
+    })
+})
+
+// promishes 
