f4b9c86c33b937703b290a3701f0410ce9a9e919
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-23 11:37:05 +0530

    code

---
+++ classesAndCallback/script.js
@@ -0,0 +1,69 @@
+// // const student = {
+// //     fullName: "kanhaiya kumar",
+// //     marks: 94.4,
+// //     printMarks: function (){
+// //         console.log("marks " + this.marks);
+// //     }
+// // }
+
+// // // prototype in js 
+// // /* 
+// // A javascript object is and entity having state and 
+// // behavior 
+// // js objects have a special property called prototype
+// // we can set prototype by using 
+// // __proto__
+// // if object & prototype have same method,
+// // object method will be used .
+// // */
+
+// // const emplooye ={
+// //     currentTex () {
+// //         console.log("Tax is 15%")
+// //     }
+// // }
+
+// // const karanArjun ={
+// //     name: karan,
+// //     salary : 50000
+// // }
+// // // use of prototype 
+// // karanArjun.__proto__ = emplooye;
+
+// class Car {
+//     constructor(brand){
+//         this.brand = brand;
+//         console.log("Creating new object ")
+//     }
+//     start(){
+//         console.log("Start")
+//     }
+    
+//     stop(){
+//         console.log("Stop")
+//     }
+
+// }
+
+// let fortuner = new Car("fortuner");
+// // constructor is a reserved word 
+
+// //inheritance in js
+// // inheritance is passing down properties 
+// // & methods from parent class to child class
+// /* 
+// class parent {
+// }
+// class child extends parent {
+// }
+// */
+// class parent {
+//     hello(){
+//         console.log("hello")
+//     }
+// }
+// class child extends parent{
+    
+// }
+
+// async programming 
