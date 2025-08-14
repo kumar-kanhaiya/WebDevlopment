705b08ebb3abb72f386a9af5838a39b6126fe5ad
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-14 19:49:00 +0530

    Add files via upload

---
+++ my-react-app/src/com.jsx
@@ -0,0 +1,19 @@
+// useEffect() = React Hook that tells React Do some code when 
+// (pick one):
+//  this component re render 
+// this component mounts
+// the state of a value 
+
+// useeffect(function , [dependencies])
+
+// 1. useeffect(() => {})  // runs after every re-render
+// 2. useeffect(() => {},[])
+// 3. useeffect(() => {}, [value]) // runs on mount + when value changes
+
+// uses 
+// Event listeners
+// 2. DOM manipulation 
+// 3. Subsriptions {real -time update }
+// 43. Fetching Data from an API
+// 5. clean up when a component unmounts
+
